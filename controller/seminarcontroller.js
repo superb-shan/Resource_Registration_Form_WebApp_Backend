ed687a78106e2c1ba27290d8d2c08b303e7695a2
Jeethula <jeethula10@gmail.com>
2023-08-07 14:49:53 +0530

    light change

--- controller/seminarcontroller.js
+++ controller/seminarcontroller.js
@@ -86,7 +86,7 @@ const GetSeminar = async (req, res) => {
 
         const result = await Seminar.findAll({ where: whereclause })
         console.log("result",result)
-        res.send(result)
+        res.send(JSON.stringify({"data":result||[]}))
 
     } catch (error) {
 
