5ff69372e20ec8f3de1ee0ca71a793c6584e34f4
Jeethula <jeethula10@gmail.com>
2023-08-11 09:31:23 +0530

    variable declare

---
+++ models/Item.js
@@ -0,0 +1,18 @@
+
+    // name, - s
+    // EmpID, - s
+    // selectedDate, - date
+    // Designation, - s
+    // Department, - s
+    // printing, - int
+    // guestMomento,
+    // studentMomento,
+    // printedEnvelop,
+    // answerBooklet,
+    // studentnotebook,
+    // studentnotebookwithgraph,
+    // studentnotebookWithoutgraph,
+    // observation, -int
+    // purpose,- s
+    // withindays, - int
+    // Ondate  - date
