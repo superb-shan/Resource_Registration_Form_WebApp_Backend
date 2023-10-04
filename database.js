42ac62004062c18e903c6403077327f843b04981
Jeethula <jeethula10@gmail.com>
2023-10-04 19:29:07 +0530

    done

--- database.js
+++ database.js
@@ -8,8 +8,8 @@ un = process.env.DB_USERNAME;
 pwd = process.env.DB_PASSWORD;
 
 
-const sequelize = new Sequlize("sql12649382", un, pwd, {
-    host: 'sql12.freesqldatabase.com',
+const sequelize = new Sequlize("db_a9fc24_jeethth", un, pwd, {
+    host: 'mysql8002.site4now.net',
     dialect: "mysql",
     logging: false,
     timezone: '+05:30',
